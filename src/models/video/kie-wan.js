c003f9b783e29d5db608b11978fc4bf531de3f15
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:24:43 +0700

    Delete src/models/video/kie-wan.js

--- src/models/video/kie-wan.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-wan', provider: 'kie', kind: 'video', label: 'Wan',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'wan',
-});
