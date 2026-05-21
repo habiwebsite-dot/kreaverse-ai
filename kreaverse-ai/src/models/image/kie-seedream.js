7d6ce32c7e3f3594f2b5c512c61424a9a72736e9
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 17:06:42 +0700

    Delete kreaverse-ai/src/models/image/kie-seedream.js

--- kreaverse-ai/src/models/image/kie-seedream.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-seedream', provider: 'kie', kind: 'image', label: 'Seedream',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'seedream',
-});
