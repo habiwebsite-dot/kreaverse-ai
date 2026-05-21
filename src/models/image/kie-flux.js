e567c0c3c8e94cd896014643f7bc08484c20574a
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:22:19 +0700

    Delete src/models/image/kie-flux.js

--- src/models/image/kie-flux.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-flux', provider: 'kie', kind: 'image', label: 'Flux',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'flux',
-});
