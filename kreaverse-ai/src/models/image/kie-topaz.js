cbf4fd26238c36250a9387dbdf795ce4f365f817
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 17:06:43 +0700

    Delete kreaverse-ai/src/models/image/kie-topaz.js

--- kreaverse-ai/src/models/image/kie-topaz.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-topaz', provider: 'kie', kind: 'image', label: 'Topaz Upscale',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'topaz',
-});
