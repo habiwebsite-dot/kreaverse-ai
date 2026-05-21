de2d3ac415ba0ba4c403d4afbf219d2bc9c67f3d
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:24:40 +0700

    Delete src/models/video/kie-runway.js

--- src/models/video/kie-runway.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-runway', provider: 'kie', kind: 'video', label: 'Runway',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'runway',
-});
