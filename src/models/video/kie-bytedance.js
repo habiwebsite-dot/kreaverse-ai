a3e71241a6e7dc5e64bed0d5b9c8141ca7910108
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:24:35 +0700

    Delete src/models/video/kie-bytedance.js

--- src/models/video/kie-bytedance.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-bytedance', provider: 'kie', kind: 'video', label: 'ByteDance',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'bytedance',
-});
