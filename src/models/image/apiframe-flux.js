0a41cf72dcec28504ee34f5407791e32564fb436
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:22:12 +0700

    Delete src/models/image/apiframe-flux.js

--- src/models/image/apiframe-flux.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'apiframe-flux', provider: 'apiframe', kind: 'image', label: 'Flux',
-  baseURL: 'https://api.apiframe.ai', envKey: 'APIFRAME_API_KEY', modelName: 'flux',
-});
