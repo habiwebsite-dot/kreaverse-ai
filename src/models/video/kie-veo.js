10d57c4e9bd593e5c1c68cecf4c76acfe772ab8e
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:24:42 +0700

    Delete src/models/video/kie-veo.js

--- src/models/video/kie-veo.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-veo', provider: 'kie', kind: 'video', label: 'Veo',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'veo',
-});
