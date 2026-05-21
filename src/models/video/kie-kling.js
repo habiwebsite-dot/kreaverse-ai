ac42e20f5de61524780e15ef9f409f7f1f9cd237
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-21 23:24:37 +0700

    Delete src/models/video/kie-kling.js

--- src/models/video/kie-kling.js
+++
@@ -1,5 +0,0 @@
-const { createAdapter } = require('../_factory');
-module.exports = createAdapter({
-  id: 'kie-kling', provider: 'kie', kind: 'video', label: 'Kling',
-  baseURL: 'https://api.kie.ai', envKey: 'KIE_AI_API_KEY', modelName: 'kling',
-});
