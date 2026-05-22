3223a5fa7d67a960d2286805fbd4a79eed6ec820
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-22 09:30:41 +0700

    Update src/utils/response.js

---
+++ src/utils/response.js
@@ -0,0 +1,9 @@
+function success(res, data, message = 'OK') {
+  res.json({ success: true, message, data });
+}
+
+function error(res, message, statusCode = 400) {
+  res.status(statusCode).json({ success: false, message });
+}
+
+module.exports = { success, error };
