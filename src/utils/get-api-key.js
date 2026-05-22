c0cfc9eda4f8d39db4366cc93761c0d7792f4055
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-22 19:53:21 +0700

    Delete src/utils/get-api-key.js

--- src/utils/get-api-key.js
+++
@@ -1,9 +0,0 @@
-function getApiKey(provider, overrideKey) {
-  if (overrideKey) return overrideKey;
-  const envKeys = process.env.PROVIDER_API_KEYS ? JSON.parse(process.env.PROVIDER_API_KEYS) : {};
-  const keyName = provider.replace(/-/g, '_');
-  const keysStr = envKeys[keyName] || '';
-  const keys = keysStr.split(',').filter(Boolean);
-  return keys[0] || '';
-}
-module.exports = { getApiKey };
