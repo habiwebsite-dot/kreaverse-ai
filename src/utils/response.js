f305d1b50575e9972938a88627d152f4a81664bd
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-22 19:53:23 +0700

    Delete src/utils/response.js

--- src/utils/response.js
+++
@@ -1,3 +0,0 @@
-function success(res, data, message = 'OK') { res.json({ success: true, message, data }); }
-function error(res, message, statusCode = 400) { res.status(statusCode).json({ success: false, message }); }
-module.exports = { success, error };
