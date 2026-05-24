ee08566458af5cdfc493a4c0faad158474ba7d44
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-24 09:56:31 +0700

    Update src/app/tools/music/page.tsx

---
+++ src/app/tools/music/page.tsx
@@ -0,0 +1,9 @@
+import { MusicGeneratorForm } from '@/components/forms/music-generator-form';
+
+export default function MusicToolPage() {
+  return (
+    <div className="container py-8">
+      <MusicGeneratorForm />
+    </div>
+  );
+}
