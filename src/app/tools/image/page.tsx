d5ae8aa589a76a0dd2cfd0796c31574085765987
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-24 09:56:27 +0700

    Update src/app/tools/image/page.tsx

---
+++ src/app/tools/image/page.tsx
@@ -0,0 +1,9 @@
+import { ImageGeneratorForm } from '@/components/forms/image-generator-form';
+
+export default function ImageToolPage() {
+  return (
+    <div className="container py-8">
+      <ImageGeneratorForm />
+    </div>
+  );
+}
