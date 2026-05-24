7af774e34844f432e5c02099294ad763713201f5
habiwebsite-dot <habiwebsite@gmail.com>
2026-05-24 09:56:21 +0700

    Update src/app/login/page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginScreen } from '@/components/login-screen';
+
+export default function LoginPage() {
+  return (
+    <div className="container py-16">
+      <LoginScreen />
+    </div>
+  );
+}
